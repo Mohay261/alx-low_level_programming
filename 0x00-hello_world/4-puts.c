9663514d45be3f204da186962fc6b1c802c7381d
mohay <mohayzew@gmail.com>
2023-07-14 01:44:50 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
-/* this is a comment */
+/**
+ * this is the entry point of the program
+ *
+ *
+ *
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
